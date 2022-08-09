829107d6a6ebe227fe83ef0d15138168cc6487ea
guru1112001 <107809909+guru1112001@users.noreply.github.com>
2022-08-09 20:20:49 +0530

    change the comment in signal.py

--- canteen/food/signals.py
+++ canteen/food/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save #post save "save the object at that time"
 from django.contrib.auth.models import User
 from .models import Customer, Order
-# this function send a signals whenever your is created 
+# this function send a signals whenever customer  is created 
 #  instance is the actual object that has just been saved and triggered the post_save signal
 
 def Customer_profile(sender, instance, created, **kwargs):
